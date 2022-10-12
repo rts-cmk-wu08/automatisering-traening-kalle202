af08f83ed71444603d9e3f963519368ed2dfa34b
[Kjartan Roberts] <[kjartanuroberts@gmail.com]>
2022-10-12 11:05:23 +0200

    setup configured

---
+++ src/components/section.js
@@ -0,0 +1,13 @@
+let section = function() {
+
+let element = document.createElement('section')
+
+element.innerHTML = `
+    <h1>Our Advantages</h1>
+`
+console.log(element)
+
+return element
+}
+
+export default section
